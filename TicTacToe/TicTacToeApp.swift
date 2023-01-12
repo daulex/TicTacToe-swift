9e622d648469ce033998237a49b7221b3e5aee00
Kirill G <kirillgalenko@gmail.com>
2023-01-12 09:27:13 +0200

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Kirills Galenko on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
